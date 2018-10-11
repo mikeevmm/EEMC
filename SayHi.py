978a8ed2107e39e53231d042301c3fcaed65ad52
Miguel M <miguelmurca@gmail.com>
2018-10-11 15:19:35 +0100

    idk

--- SayHi.py
+++ SayHi.py
@@ -4,12 +4,13 @@ import socket
 import threading
 import time
 import random
-from itertools import chain
+import re
 
 class Channel(object):
 	WHOAMI = 0
 	PROBEOUT = 1
 	PROBEIN = 2
+	TEXTMSG = 3
 
 	"""
 	Payload structures:
@@ -22,17 +23,21 @@ class Channel(object):
 		Request for the contact list of the peer
 	
 	PROBEIN:
-		[2 bytes; payload ID] [1 bytes (unsigned); uint count of incoming peer address count ] [remainder; (ip address, port) in groups of 4+2 bytes]
+		[2 bytes; payload ID] [1 bytes (unsigned); uint count of incoming peer address count ] [remainder; (ip address, port, human_name) in groups of 4+2+8 bytes]
 		Response to PROBEOUT request from a queried peer
 	"""
 
-	def __init__(self, in_port):
+	def __init__(self, in_port, human_name):
 		self.channel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.channel.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 		self.channel.bind(("", in_port))
 		self.channel.settimeout(0)
 		self.channel_lock = threading.Lock()
 		self.port = in_port
+		self.human_name = human_name
+		if len(human_name) > 8:
+			print("Human name truncated to 8 characters")
+			self.human_name = self.human_name[:8]
 
 		self.addr_secret = random.getrandbits(32).to_bytes(4, byteorder='big')
 		self.addr = None
@@ -46,7 +51,12 @@ class Channel(object):
 		self.listen_thread = threading.Thread(target=self._thread_listen_)
 		self.listen_thread.start()
 
-		self._find_addr_()
+		while self.addr is None:
+			id = self.identifier(self.WHOAMI) 	# 2 bytes
+			random_key = self.addr_secret		# 4 bytes
+			byte_digest = id + random_key
+			self.broadcast(byte_digest)
+			time.sleep(1)
 
 		self.probe_thread = threading.Thread(target=self._thread_probe_)
 		self.probe_thread.start()
@@ -72,16 +82,13 @@ class Channel(object):
 				self.channel_lock.release()
 			if None not in (msg, addr):
 				self._on_msg_(msg, addr, port)
-			time.sleep(0.1)
 	
 	def _thread_probe_(self):
 		while(self.alive):
 			if len(self.contact_list) == 0:
 				print("No contacts, bootstrapping through <broadcast>")
-				self.channel_lock.acquire()
-				self._send_probeout_('<broadcast>', self.port, True)
-				self.channel_lock.release()
-			time.sleep(0.1)
+				self._send_probeout_('', self.port, True)
+			time.sleep(3)
 
 	def _on_msg_(self, msg, addr, port):
 		msg = bytearray(msg)
@@ -95,18 +102,21 @@ class Channel(object):
 				self.addr = bytes(map(int, addr.split('.')))
 
 		elif id == self.PROBEOUT: # External request for contacts
-			broadcast_mode = bool(int.from_bytes(payload[0], byteorder='big', signed=False))
+			if addr == self.addr_human:
+				return
+			print("Recieved PROBEOUT")
+			broadcast_mode = payload[0]
 			if not broadcast_mode and len(self.contact_list) == 0:
 				return
 			return_list = self.contact_list
 			if broadcast_mode:
-				return_list.add((self.addr, self.port))
+				return_list.add((self.addr, self.port, self.human_name))
 			if addr in return_list:
 				return_list.remove(addr)
 			
 			requesting_port = int.from_bytes(payload[1:3], byteorder='big', signed=False)
 			contacts_len_digest = int(len(return_list)).to_bytes(1, byteorder='big', signed=False)
-			contacts_digest = bytearray(return_list)
+			contacts_digest = bytearray(*((x[0] + int(x[1]).to_bytes(2, byteorder='big', signed=False) + bytes(self.human_name, 'ascii')) for x in return_list))
 			response_id = self.identifier(self.PROBEIN)
 			
 			print("Got PROBEOUT request from {}, sending PROBEIN back".format(addr))
@@ -114,10 +124,17 @@ class Channel(object):
 			self.send(full_response, addr, requesting_port) # Send PROBEIN back
 
 		elif id == self.PROBEIN: # Response to contacts request
-			contact_count = int.from_bytes(payload[0], byteorder='big', signed=False)
-			contacts = set((payload[i:i+4], payload[i+4:i+6]) for i in range(1, len(payload[1:]), 6))
+			print("Recieved PROBEIN")
+			contact_count = payload[0]
+
+			contacts = set()
+			for i in range(1, len(payload[1:]), 4+2+8):
+				contact_ip = bytes(payload[i:i+4])
+				contact_port = int.from_bytes(payload[i+4:i+6], byteorder='big', signed=False)
+				contact_name = bytes(payload[i+6:i+14]).decode('ascii')
+
 			new_contacts = contacts - self.contact_list
-			self.contact_list.add(new_contacts)
+			self.contact_list.union(new_contacts)
 
 			print("Got PROBEIN response with {} new elements.".format(contact_count))
 			for contact in new_contacts: # Send PROBEOUT to new contacts
@@ -126,28 +143,30 @@ class Channel(object):
 
 	def _send_probeout_(self, to_addr, to_port, broadcast=False):
 		probeout_id = self.identifier(self.PROBEOUT)
-		broadcast_mode = (1 if broadcast else 0).to_bytes(1, byteorder='big', signed=False)
+		broadcast_mode = b'\x01' if broadcast else b'\x00'
 		listening_on = self.port.to_bytes(2, byteorder='big', signed=False)
-		self.send(probeout_id + broadcast_mode + listening_on, to_addr, to_port)
-
-	def _find_addr_(self):
-		id = self.identifier(self.WHOAMI) 	# 2 bytes
-		random_key = self.addr_secret		# 4 bytes
-		byte_digest = id + random_key
-		self.broadcast(byte_digest)
+		msg_digest = probeout_id + broadcast_mode + listening_on
+		if broadcast:
+			self.broadcast(msg_digest, to_port)
+		else:
+			self.send(msg_digest, to_addr, to_port)
 	
 	def identifier(self, id_int):
 		return id_int.to_bytes(2, byteorder='big', signed = False)
 
-	def broadcast(self, msg):
-		self.send(msg, '<broadcast>', self.port)
+	def broadcast(self, msg, port = None):
+		if port is None:
+			port = self.port
+		self.send(msg, '<broadcast>', port)
 
 	def send(self, msg, addr, addr_port):
 		self.channel_lock.acquire()
 		self.channel.sendto(msg, (addr, addr_port))
 		self.channel_lock.release()
 
+mention_reg
 
-with Channel(6969) as channel:
+name = input("NAME>").strip().lower()
+with Channel(1337, name) as channel:
 	while True:
-		pass
+		msg = input(">").strip()
